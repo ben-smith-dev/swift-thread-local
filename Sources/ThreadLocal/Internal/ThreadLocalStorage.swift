f00ba5f73ed49b4f1744aa755214ac013fc68aea
Ben <23388317+ben-smith-dev@users.noreply.github.com>
2026-04-28 17:31:40 -0500

    Add 'ThreadLocalStorage' struct.
    Uses pthread API to store + retrieve thread specific values. Method calls are limited to syncronous contexts due to possible thread switching in async contexts.

---
+++ Sources/ThreadLocal/Internal/ThreadLocalStorage.swift
@@ -0,0 +1,106 @@
+import pthread
+
+internal struct ThreadLocalStorage<Value>: ~Copyable {
+    private let key: pthread_key_t
+
+    public init() {
+        var key: pthread_key_t = 0
+        let errorCode: Int32 = pthread_key_create(&key) { pointer in
+            Unmanaged<AnyObject>.fromOpaque(pointer).release()
+        }
+
+        precondition(
+            errorCode == .zero,
+            "Failed to create thread specific value key, error: \(errorCode)."
+        )
+
+        self.key = key
+    }
+
+    @available(*, noasync)
+    public func get() -> Value? {
+        guard let box: MutableBox = self.getBox() else {
+            return nil
+        }
+
+        return box.value
+    }
+
+    @available(*, noasync)
+    public func set(_ value: Value) {
+        if let box: MutableBox = self.getBox() {
+            box.value = value
+        } else {
+            self.setBox(MutableBox(value))
+        }
+    }
+
+    @available(*, noasync)
+    public func clear() {
+        guard let pointer = pthread_getspecific(self.key) else {
+            return
+        }
+
+        let errorCode: Int32 = pthread_setspecific(self.key, nil)
+        precondition(
+            errorCode == .zero,
+            "Failed to remove thread specific value box, error: \(errorCode)."
+        )
+
+        Unmanaged<AnyObject>.fromOpaque(pointer).release()
+    }
+
+    @available(*, noasync)
+    private func getBox() -> MutableBox? {
+        guard let pointer: UnsafeMutableRawPointer = pthread_getspecific(self.key) else {
+            return nil
+        }
+
+        let box: MutableBox = Unmanaged<MutableBox>
+            .fromOpaque(pointer)
+            .takeUnretainedValue()
+
+        return box
+    }
+
+    @available(*, noasync)
+    private func setBox(_ box: MutableBox) {
+        assert(pthread_getspecific(self.key) == nil)
+
+        let pointer: UnsafeMutableRawPointer = Unmanaged
+            .passRetained(box)
+            .toOpaque()
+
+        let errorCode: Int32 = pthread_setspecific(self.key, pointer)
+        precondition(
+            errorCode == .zero,
+            "Failed to set thread specific value box, error: \(errorCode)."
+        )
+    }
+
+    deinit {
+        let errorCode: Int32 = pthread_key_delete(self.key)
+        precondition(
+            errorCode == .zero,
+            "Failed to delete thread specific value key, error: \(errorCode)."
+        )
+    }
+}
+
+// MARK: Mutable Box
+
+extension ThreadLocalStorage {
+    private final class MutableBox {
+        public var value: Value
+
+        public init(_ value: Value) {
+            self.value = value
+        }
+    }
+}
+
+// MARK: Sendable Conformance
+
+extension ThreadLocalStorage: Sendable where Value: Sendable {
+    // Empty.
+}
