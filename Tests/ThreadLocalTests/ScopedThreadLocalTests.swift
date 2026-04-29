129808519ef41f097543525de85cd17644003568
Ben <23388317+ben-smith-dev@users.noreply.github.com>
2026-04-29 14:10:19 -0500

    Add 'ScopedThreadLocalTests' test cases.

--- Tests/ThreadLocalTests/ScopedThreadLocalTests.swift
+++ Tests/ThreadLocalTests/ScopedThreadLocalTests.swift
@@ -31,6 +31,25 @@ private struct ScopedThreadLocalTests {
     }
 
     @Test
+    private func sequentialUnscopedGetProvidedSameInstance() {
+        let threadLocalValue = ScopedThreadLocal(defaultValue: Ref())
+
+        // swiftlint:disable:next identical_operands
+        #expect(threadLocalValue.get() === threadLocalValue.get())
+    }
+
+    @Test
+    private func sequentialScopedGetProvidedSameInstance() {
+        let scopedValue = Ref()
+        let threadLocalValue = ScopedThreadLocal(defaultValue: Ref())
+
+        threadLocalValue.withValue(scopedValue) {
+            #expect(threadLocalValue.get() === scopedValue)
+            #expect(threadLocalValue.get() === scopedValue)
+        }
+    }
+
+    @Test
     private func withValueProperlyScopesValueAfterUnscopedGet() throws {
         let defaultValue: Int = 0
         let scopedValue: Int = 1
@@ -68,6 +87,28 @@ private struct ScopedThreadLocalTests {
     }
 
     @Test
+    private func withValueRestoresPreviousValueIfOperationThrows() throws {
+        let defaultValue: Int = 0
+        let scopedValue: Int = 1
+        let nestedScopedValue: Int = 2
+        let threadLocalValue = ScopedThreadLocal(defaultValue: defaultValue)
+
+        try threadLocalValue.withValue(scopedValue) {
+            try #require(threadLocalValue.get() == scopedValue)
+
+            try #require(throws: StubError.self) {
+                try threadLocalValue.withValue(nestedScopedValue) {
+                    try #require(threadLocalValue.get() == nestedScopedValue)
+
+                    throw StubError()
+                }
+            }
+
+            #expect(threadLocalValue.get() == scopedValue)
+        }
+    }
+
+    @Test
     private func eachThreadGetsItsOwnDefaultValue() async {
         let threadLocalValue = ScopedThreadLocal(defaultValue: Ref())
 
@@ -82,6 +123,10 @@ private struct ScopedThreadLocalTests {
 
 extension ScopedThreadLocalTests {
     private final class Ref: Sendable {
+        // Empty.
+    }
+
+    private struct StubError: Error {
         // Empty.
     }
 }
